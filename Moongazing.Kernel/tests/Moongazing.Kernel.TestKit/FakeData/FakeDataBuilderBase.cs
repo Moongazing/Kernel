845f42e44aa3c123b7f8f88fb6bfad17c94e120d
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-07 13:03:30 +0100

    Add Moongazing.Kernel.TestKit project and related classes
    Updated Moongazing.Kernel.sln to include the new Moongazing.Kernel.TestKit project. Added Moongazing.Kernel.TestKit.csproj targeting .NET 9.0 with references to AutoMapper, Moq, and Moongazing.Kernel.Persistence. Introduced ValidationErrorCodes, FakeDataBuilderBase, MockRepositoryHelper, and BaseMockRepository classes to support testing.

---
+++ Moongazing.Kernel/tests/Moongazing.Kernel.TestKit/FakeData/FakeDataBuilderBase.cs
@@ -0,0 +1,11 @@
+﻿using Moongazing.Kernel.Persistence.Repositories.Common;
+
+namespace Moongazing.Kernel.TestKit.FakeData;
+
+public abstract class FakeDataBuilderBase<TEntity, TEntityId>
+    where TEntity : Entity<TEntityId>, new()
+{
+    public List<TEntity> Data => CreateFakeData();
+    public abstract List<TEntity> CreateFakeData();
+}
+
