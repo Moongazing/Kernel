b9baf9826ff8b275c4d88f9a9cbd6aac072bc8ba
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-01-07 14:48:07 +0100

    Add MediatR behaviors and request interfaces
    Updated Moongazing.Kernel.Application.csproj to include new package references for FluentValidation, MediatR, Microsoft.AspNetCore.Http.Abstractions, and Polly. Added project references to Moongazing.Kernel.CrossCuttingConcerns and Moongazing.Kernel.Security.
    
    Introduced various MediatR behaviors: AuthorizationBehavior, CacheRemovingBehavior, CachingBehavior, CircuitBreakerBehavior, LoggingBehavior, PerformanceBehavior, RateLimitingBehavior, TransactionScopeBehavior, and RequestValidationBehavior.
    
    Added interfaces to define contracts for requests: ISecuredRequest, ICachableRequest, ICacheRemoverRequest, ICircuitBreakerRequest, ILoggableRequest, IIntervalRequest, IRateLimitedRequest, ITransactionalRequest, IBulkResponse, and IResponse.
    
    Introduced CacheSettings, ValidationTool, PageRequest, and GetListResponse classes.

---
+++ Moongazing.Kernel/src/Moongazing.Kernel.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Moongazing.Kernel.CrossCuttingConcerns.Logging;
+using Moongazing.Kernel.CrossCuttingConcerns.Logging.Serilog;
+using System.Text.Json;
+
+namespace Moongazing.Kernel.Application.Pipelines.Logging;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+     where TRequest : IRequest<TResponse>, ILoggableRequest
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+    private readonly LoggerServiceBase loggerServiceBase;
+
+    public LoggingBehavior(IHttpContextAccessor httpContextAccessor, LoggerServiceBase loggerServiceBase)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+        this.loggerServiceBase = loggerServiceBase;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        List<LogParameter> logParameters =
+        [
+            new LogParameter
+            {
+                Type = request.GetType().Name,
+                Value = request
+            }
+        ];
+
+        LogDetail logDetail = new()
+        {
+            MethodName = next.Method.Name,
+            Parameters = logParameters,
+            User = httpContextAccessor.HttpContext?.User.Identity?.Name ?? "?"
+        };
+
+        loggerServiceBase.Info(JsonSerializer.Serialize(logDetail));
+        return await next();
+    }
+}
